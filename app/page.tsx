d7ae3181a3a58692a01a7e92e74d95180bbec043
Mayura Vivekananda <km.vivekananda@gmail.com>
2025-03-23 17:03:19 -0400

    Added import/export function

--- app/page.tsx
+++ app/page.tsx
@@ -55,7 +55,7 @@ export default async function Home() {
                 <DaySchedule
                   day={Number(day)}
                   activities={schedule[Number(day)] || []}
-                  defaultShowTimescale={true}
+                  defaultShowTimescale={false}
                 />
               </TabsContent>
             ))}
